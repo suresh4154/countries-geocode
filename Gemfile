256a7878f9e5f61b596bed589c19d9f4bcf2e5ff
suresh <suresh_torlapati@yahoo.com>
2012-09-12 20:23:19 +0530

    Heroku Gemfile fix

--- Gemfile
+++ Gemfile
@@ -4,7 +4,10 @@ gem 'rails', '3.2.6'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
-gem 'pg'
+group :production do
+      gem 'thin'
+      gem 'pg'
+    end
 gem 'mysql'
 gem "twitter-bootstrap-rails", :group => :assets
 gem 'devise'
--- lib/myplugins/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Geocode::Application.config.secret_token = 'f5e31fc896068fbe1e8ec52ed26d8f6bbae21da4a9981cf927ede1a4ace115b0c61bb9abfc89ddcd7eb5e6a872a713ea005878fd8fbd7ef9366e19a39bc7c05b'
